7668fc93fff7cf0032657445fe74f44d2b01fd08
galakerman <akermang@gmail.com>
2017-09-02 19:42:58 +0300

    mock data added

---
+++ js/mock/lessons.js
@@ -0,0 +1,20 @@
+var lessons = [
+    {
+        date_created: new Date().getTime(),
+        student_ids: ["01","02"],
+        subject: "sonata",
+        music_sheet_url: "http://www.piano-play-it.com/images/moonlight-sonata-pic.gif",
+        video_url: 'https://www.youtube.com/embed/SiufFlbr7p8',  
+        info: "good luck",
+        tutorial_url: 'https://www.youtube.com/embed/SiufFlbr7p8'
+    },
+    {
+        date_created: new Date().getTime(),
+        student_ids: ["03","04"],
+        subject: "song",
+        music_sheet_url: "http://www.piano-play-it.com/images/moonlight-sonata-pic.gif",
+        video_url: 'https://www.youtube.com/embed/SiufFlbr7p8',  
+        info: "work hard and get succsefull",
+        tutorial_url: 'https://www.youtube.com/embed/SiufFlbr7p8'
+    }
+]
---
+++ js/mock/students.js
@@ -0,0 +1,22 @@
+var students = [
+    {
+        id: "01",
+        first_name: "gal",
+        last_name: "ackerman"
+    },
+    {
+        id: "02",
+        first_name: "meir",
+        last_name: "aharony"
+    },
+    {
+        id: "03",
+        first_name: "gil",
+        last_name: "yohanan"
+    },
+    {
+        id: "04",
+        first_name: "yosy",
+        last_name: "Avramov"
+    }
+]
