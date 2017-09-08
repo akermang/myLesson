0e951f1c9cc901fb352ad714b59e71e9920ecea0
galakerman <akermang@gmail.com>
2017-09-08 23:44:17 +0300

    api --> function get , setIfNull

--- js/api.js
+++ js/api.js
@@ -1,18 +1,27 @@
 function getLessons(){
-    var storedLessons = localStorage.getItem("lessons_data");
-    if(storedLessons == null){
-        localStorage.setItem("lessons_data", JSON.stringify(lessonsMockData));
-        return lessonsMockData;
-    }
-    return JSON.parse(storedLessons);
+    var key = "lessons_data";
+    var storedLessons = getItem(key);
+    return storeIfNull(storedLessons, lessonsMockData, key);
 };
-
 
 function getStudends(){
-    var storedStudents = localStorage.getItem("students_data");
-    if(storedStudents == null){
-        localStorage.setItem("students_data", JSON.stringify(studentsMockData));
-        return studentsMockData;
-    }
-    return JSON.parse(storedStudents);
+    var key = "students_data";
+    var storedStudents = getItem(key);
+    return storeIfNull(storedStudents, studentsMockData, key);
 };
+
+function getItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function storeInDb(data, key) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+function storeIfNull(storedData, mockData, key) {
+    if(!storedData){
+        storeInDb(mockData, key);
+        return mockData;
+    }
+    return storedData;
+}
