b58447bb109d326426c8a50bec81f9f1ece0a415
galakerman <akermang@gmail.com>
2017-12-30 15:34:52 +0200

    innerHTML for the modal and some css work

--- js/render.js
+++ js/render.js
@@ -221,7 +221,9 @@ function renderEditedLesson(lessonContainer, lesson){
 function deleteIconClicked(lesson,lessonContainer){
     setSelectedLesson(lesson);
     lessonContainer.style.background = "rgba(190, 190, 160, 0.76)";
-    $('#exampleModal').modal('show');
+    $('.modal-title')[0].innerText = `Delete ${lesson.subject} lesson..?`;
+    $('.modal-body')[0].innerText = `Delete it will permanently delete ${lesson.subject} !`;
+    $('#exampleModal').modal('show');   
     $("#btn-cencel-delete").click(cenceleDelete.bind(this, lessonContainer));
     $("#btn-delete-lesson").click(deleteLesson);
 }
